a4397e9d9349565d4c7836d8c359b887e33af2d0
WuCian <igorjuraszek@gmail.com>
2022-05-27 19:00:57 +0200

    Adding listing contributions

--- app/home/contributions/route.js
+++ app/home/contributions/route.js
@@ -1,3 +1,11 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
-export default class HomeContributionsRoute extends Route {}
+export default class HomeContributionsRoute extends Route {
+  @service store;
+
+  model() {
+    const contributionUser = this.store.findAll('contribution-user');
+    return contributionUser;
+  }
+}
