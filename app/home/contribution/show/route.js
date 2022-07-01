485286e2b6de3985dc5df8b26ff1721b63653495
Igor Juraszek <igorjuraszek@gmail.com>
2022-07-01 14:53:07 +0200

    Showing contribution list with details

--- app/home/contribution/show/route.js
+++ app/home/contribution/show/route.js
@@ -1,3 +1,11 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
-export default class HomeContributionShowRoute extends Route {}
+export default class HomeContributionShowRoute extends Route {
+  @service store;
+
+  async model(params) {
+    const contribution = await this.store.findRecord('contribution', params.id);
+    return contribution;
+  }
+}
