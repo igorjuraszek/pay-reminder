41fb241520538e4ff81b4716b419b9296d10c084
Igor Juraszek <igorjuraszek@gmail.com>
2022-07-01 18:44:38 +0200

    Showing user details

--- app/home/user/show/route.js
+++ app/home/user/show/route.js
@@ -1,3 +1,10 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
-export default class HomeUserShowRoute extends Route {}
+export default class HomeUserShowRoute extends Route {
+  @service store;
+
+  async model({ id }) {
+    return await this.store.findRecord('user', id);
+  }
+}
