0b1554a8bba20b9f86e62b80def880f24afcb50e
Igor Juraszek <igorjuraszek@gmail.com>
2022-07-08 15:12:44 +0200

    Adding showing only mine contributions

--- app/home/contributions/history/route.js
+++ app/home/contributions/history/route.js
@@ -1,3 +1,28 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
+export default class HomeHistoryRoute extends Route {
+  @service store;
+  @service session;
 
-export default class HomeHistoryRoute extends Route {}
+  model() {
+    return this.myContributions();
+  }
+
+  async myContributions() {
+    const contributions = await this.store.findAll('contribution');
+    const currentUserId = this.session.currentUser.get('id');
+    const filteredContributions = contributions.filter((contribution) => {
+      const isContributionActive = !contribution.isClosed;
+      const amIOwner = contribution.owner.get('id') === currentUserId;
+
+      const contributors = contribution.contributors.map(({ contributor }) =>
+        contributor.get('id')
+      );
+
+      const amIContributor = contributors.includes(currentUserId);
+
+      return (amIOwner || amIContributor) && !isContributionActive;
+    });
+    return filteredContributions;
+  }
+}
