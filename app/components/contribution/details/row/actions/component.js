8113751635e66dfa91dd4135a072333dbe0f2c97
Igor Juraszek <igorjuraszek@gmail.com>
2022-07-12 17:54:21 +0200

    Fixing mailto

--- app/components/contribution/details/row/actions/component.js
+++ app/components/contribution/details/row/actions/component.js
@@ -5,6 +5,24 @@ import { action } from '@ember/object';
 export default class ContributionDetailsRowActionsComponent extends Component {
   @service store;
 
+  get mailHref() {
+    const {
+      contribution: { title },
+      receiver,
+      deadline,
+      debt: { amount },
+      owner,
+    } = this.args;
+    const subject = `Overdue payment in contribution ${title}`;
+    const body = `Hello ${receiver.get(
+      'username'
+    )}. There's an overdue payment (till ${deadline}) for amount ${amount} PLN to user ${owner}. Please pay ASAP!!!`;
+
+    return encodeURI(
+      `mailto:${receiver.get('email')}?subject=${subject}&body=${body}`
+    );
+  }
+
   async updateStatusOfContribution() {
     const { contribution } = this.args;
     const unpaidDebts = contribution.contributors.filter((debt) => {
