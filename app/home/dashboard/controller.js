4bb646e6d9a65358ab7e7b6d6bd2162325b9c1f8
Igor Juraszek <igorjuraszek@gmail.com>
2022-07-13 11:33:01 +0200

    Adding widgets examples and grid

--- app/home/dashboard/controller.js
+++ app/home/dashboard/controller.js
@@ -4,18 +4,75 @@ import { inject as service } from '@ember/service';
 export default class HomeDashboardController extends Controller {
   @service session;
   @service router;
+  @service store;
 
   get options() {
     const options = {
       title: 'My ',
-      height: 300,
-      width: 400,
+      height: 400,
+      width: 600,
 
       animation: {
-        startup: false,
+        startup: true,
         easing: 'inAndOut',
       },
     };
     return options;
   }
+
+  get data1() {
+    return [
+      ['Task', 'Hours per Day'],
+      ['Work', 21],
+      ['213', 37],
+      ['qweq', 8],
+      ['Eaca', 1],
+      ['casd', 2],
+      ['vwe', 88],
+    ];
+  }
+
+  get data2() {
+    return [
+      ['Element', 'Density', { role: 'style' }],
+      ['Copper', 8.94, '#b87333'],
+      ['Silver', 10.49, 'silver'],
+      ['Gold', 19.3, 'gold'],
+      ['Platinum', 21.45, 'color: #e5e4e2'],
+      ['Gold', 19.3, 'gold'],
+      ['Gold', 19.3, 'gold'],
+      ['Gold', 19.3, 'gold'],
+      ['Gold', 19.3, 'gold'],
+      ['Gold', 19.3, 'gold'],
+      ['Gold', 19.3, 'gold'],
+      ['Gold', 19.3, 'gold'],
+    ];
+  }
+
+  get data3() {
+    return [
+      ['Year', 'Sales', 'Expenses'],
+      ['2004', 1000, 400],
+      ['2005', 1170, 460],
+      ['2006', 660, 1120],
+      ['2007', 1030, 540],
+    ];
+  }
+
+  get contributionsIOwn() {
+    return this.model.contributionsIOwn;
+  }
+
+  get contributionsIBelongTo() {
+    return this.model.contributionsIBelongTo;
+  }
+
+  //   activeDebts;
+  //   activeDebtsAmount;
+  //   paidDebts;
+  //   paidDebtsAmount;
+  //   paymentsIAmWaitingFor;
+  //   paymentsIAmWaitingForAmount;
+  //   confirmedPayments;
+  //   confirmedPaymentsAmount;
 }
--- app/home/dashboard/route.js
+++ app/home/dashboard/route.js
@@ -1,3 +1,43 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
+import RSVP from 'rsvp';
 
-export default class HomeDashboardRoute extends Route {}
+export default class HomeDashboardRoute extends Route {
+  @service store;
+  @service session;
+
+  model() {
+    return RSVP.hash({
+      contributionsIOwn: this.contributionsIOwn(),
+      contributionsIBelongTo: this.contributionsIBelongTo(),
+    });
+  }
+
+  async contributionsIOwn() {
+    const contributions = await this.store.findAll('contribution');
+    const currentUserId = this.session.currentUser.get('id');
+    const filteredContributions = contributions.filter((contribution) => {
+      const amIOwner = contribution.owner.get('id') === currentUserId;
+
+      return amIOwner;
+    });
+    return filteredContributions;
+  }
+
+  async contributionsIBelongTo() {
+    const contributions = await this.store.findAll('contribution');
+    const currentUserId = this.session.currentUser.get('id');
+    const filteredContributions = contributions.filter((contribution) => {
+      const amIOwner = contribution.owner.get('id') === currentUserId;
+
+      const contributors = contribution.contributors.map(({ contributor }) =>
+        contributor.get('id')
+      );
+
+      const amIContributor = contributors.includes(currentUserId);
+
+      return amIContributor && !amIOwner;
+    });
+    return filteredContributions;
+  }
+}
