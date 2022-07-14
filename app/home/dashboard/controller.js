5936b4ea0b67c68990f99999cc660a65dafa9492
Igor Juraszek <igorjuraszek@gmail.com>
2022-07-14 11:26:10 +0200

    Finishing all getters

--- app/home/dashboard/controller.js
+++ app/home/dashboard/controller.js
@@ -1,5 +1,7 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
+import { sum } from 'lodash';
+import { getYear, getMonth } from 'date-fns';
 
 export default class HomeDashboardController extends Controller {
   @service session;
@@ -67,12 +69,107 @@ export default class HomeDashboardController extends Controller {
     return this.model.contributionsIBelongTo;
   }
 
-  //   activeDebts;
-  //   activeDebtsAmount;
-  //   paidDebts;
-  //   paidDebtsAmount;
-  //   paymentsIAmWaitingFor;
-  //   paymentsIAmWaitingForAmount;
-  //   confirmedPayments;
-  //   confirmedPaymentsAmount;
+  get myDebts() {
+    const myDebts = this.contributionsIBelongTo.map((contribution) => {
+      return contribution.contributors.filter(
+        (debt) =>
+          debt.contributor.get('id') === this.session.currentUser.get('id')
+      ).firstObject;
+    });
+    return myDebts;
+  }
+
+  get activeDebts() {
+    return this.myDebts.filter((debt) => {
+      return !debt.isPaid;
+    });
+  }
+
+  get paidDebts() {
+    return this.myDebts.filter((debt) => {
+      return debt.isPaid;
+    });
+  }
+
+  get activeDebtsAmount() {
+    return sum(
+      this.activeDebts.map((debt) => {
+        return debt.amount;
+      })
+    );
+  }
+
+  get paidDebtsAmount() {
+    return sum(
+      this.paidDebts.map((debt) => {
+        return debt.amount;
+      })
+    );
+  }
+
+  get paymentsIAmWaitingFor() {
+    return this.contributionsIOwn
+      .map((contribution) => {
+        return contribution.contributors.filter((contributor) => {
+          return !contributor.isPaid;
+        });
+      })
+      .flat();
+  }
+
+  get paymentsIAmWaitingForAmount() {
+    return sum(
+      this.paymentsIAmWaitingFor.map((payment) => {
+        return payment.amount;
+      })
+    );
+  }
+
+  get confirmedPayments() {
+    return this.contributionsIOwn
+      .map((contribution) => {
+        return contribution.contributors.filter((contributor) => {
+          return contributor.isPaid;
+        });
+      })
+      .flat();
+  }
+
+  get confirmedPaymentsAmount() {
+    return sum(
+      this.confirmedPayments.map((payment) => {
+        return payment.amount;
+      })
+    );
+  }
+
+  get currentYearOwnedContributions() {
+    const currentYear = getYear(new Date());
+    const contributionsInCurrentYear = this.contributionsIOwn.filter(
+      (contribution) => {
+        return getYear(contribution.createdAd) === currentYear;
+      }
+    );
+    const contributionsByMonth = Array.from(Array(12).keys()).map((index) => {
+      return contributionsInCurrentYear.filter((contribution) => {
+        return getMonth(contribution.createdAd) === index + 1;
+      });
+    });
+    return contributionsByMonth;
+  }
+
+  get currentYearBelongsContributions() {
+    const currentYear = getYear(new Date());
+    const contributionsInCurrentYear = this.contributionsIBelongTo.filter(
+      (contribution) => {
+        return getYear(contribution.createdAd) === currentYear;
+      }
+    );
+    const contributionsByMonth = Array.from(Array(12).keys()).map((index) => {
+      return contributionsInCurrentYear.filter((contribution) => {
+        return getMonth(contribution.createdAd) === index + 1;
+      });
+    });
+    return contributionsByMonth;
+  }
 }
