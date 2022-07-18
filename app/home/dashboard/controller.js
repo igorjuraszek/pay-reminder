3bc76ed9d8d9a7e9a428428be155dbc3ff9b52bf
Igor Juraszek <igorjuraszek@gmail.com>
2022-07-18 13:26:49 +0200

    Destructurize

--- app/home/dashboard/controller.js
+++ app/home/dashboard/controller.js
@@ -17,8 +17,8 @@ export default class HomeDashboardController extends Controller {
   }
 
   get myDebts() {
-    const myDebts = this.contributionsIBelongTo.map((contribution) => {
-      return contribution.contributors.filter(
+    const myDebts = this.contributionsIBelongTo.map(({ contributors }) => {
+      return contributors.filter(
         (debt) =>
           debt.contributor.get('id') === this.session.currentUser.get('id')
       ).firstObject;
@@ -27,80 +27,56 @@ export default class HomeDashboardController extends Controller {
   }
 
   get activeDebts() {
-    return this.myDebts.filter((debt) => {
-      return !debt.isPaid;
-    });
+    return this.myDebts.filter(({ isPaid }) => !isPaid);
   }
 
   get paidDebts() {
-    return this.myDebts.filter((debt) => {
-      return debt.isPaid;
-    });
+    return this.myDebts.filter(({ isPaid }) => isPaid);
   }
 
   get activeDebtsAmount() {
-    return sum(
-      this.activeDebts.map((debt) => {
-        return debt.amount;
-      })
-    );
+    return sum(this.activeDebts.map(({ amount }) => amount));
   }
 
   get paidDebtsAmount() {
-    return sum(
-      this.paidDebts.map((debt) => {
-        return debt.amount;
-      })
-    );
+    return sum(this.paidDebts.map(({ amount }) => amount));
   }
 
   get paymentsIAmWaitingFor() {
     return this.contributionsIOwn
-      .map((contribution) => {
-        return contribution.contributors.filter((contributor) => {
-          return !contributor.isPaid;
+      .map(({ contributors }) => {
+        return contributors.filter(({ isPaid }) => {
+          return !isPaid;
         });
       })
       .flat();
   }
 
   get paymentsIAmWaitingForAmount() {
-    return sum(
-      this.paymentsIAmWaitingFor.map((payment) => {
-        return payment.amount;
-      })
-    );
+    return sum(this.paymentsIAmWaitingFor.map(({ amount }) => amount));
   }
 
   get confirmedPayments() {
     return this.contributionsIOwn
-      .map((contribution) => {
-        return contribution.contributors.filter((contributor) => {
-          return contributor.isPaid;
-        });
+      .map(({ contributors }) => {
+        return contributors.filter(({ isPaid }) => isPaid);
       })
       .flat();
   }
 
   get confirmedPaymentsAmount() {
-    return sum(
-      this.confirmedPayments.map((payment) => {
-        return payment.amount;
-      })
-    );
+    return sum(this.confirmedPayments.map(({ amount }) => amount));
   }
 
   get currentYearOwnedContributions() {
     const currentYear = getYear(new Date());
     const contributionsInCurrentYear = this.contributionsIOwn.filter(
-      (contribution) => {
-        return getYear(contribution.createdAd) === currentYear;
-      }
+      ({ createdAd }) => getYear(createdAd) === currentYear
     );
     const contributionsByMonth = Array.from(Array(12).keys()).map((index) => {
-      return contributionsInCurrentYear.filter((contribution) => {
-        return getMonth(contribution.createdAd) === index + 1;
-      });
+      return contributionsInCurrentYear.filter(
+        ({ createdAd }) => getMonth(createdAd) === index + 1
+      );
     });
     return contributionsByMonth;
   }
@@ -108,14 +84,12 @@ export default class HomeDashboardController extends Controller {
   get currentYearBelongsContributions() {
     const currentYear = getYear(new Date());
     const contributionsInCurrentYear = this.contributionsIBelongTo.filter(
-      (contribution) => {
-        return getYear(contribution.createdAd) === currentYear;
-      }
+      ({ createdAd }) => getYear(createdAd) === currentYear
     );
     const contributionsByMonth = Array.from(Array(12).keys()).map((index) => {
-      return contributionsInCurrentYear.filter((contribution) => {
-        return getMonth(contribution.createdAd) === index + 1;
-      });
+      return contributionsInCurrentYear.filter(
+        ({ createdAd }) => getMonth(createdAd) === index + 1
+      );
     });
     return contributionsByMonth;
   }
