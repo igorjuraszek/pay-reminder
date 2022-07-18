0be9bd67880700840740209f2c302061f075f4c2
Igor Juraszek <igorjuraszek@gmail.com>
2022-07-18 16:08:21 +0200

    Testing navbar

---
+++ tests/integration/components/account-settings/component-test.js
@@ -0,0 +1,159 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render, click, fillIn, waitFor } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+module('Integration | Component | account-settings/details', function (hooks) {
+  setupRenderingTest(hooks);
+
+  hooks.beforeEach(async function () {
+    const user = {
+      username: 'konon17',
+      name: 'Krzysztof',
+      surname: 'Kononowicz',
+      password: 'Szkolna17',
+      email: 'kononowiczkrzysztof@bio.pl',
+      blikNumber: '123456789',
+      revolutUsername: 'konon',
+    };
+    this.set('currentUser', user);
+
+    await render(
+      hbs`<AccountSettings::Details @currentUser={{this.currentUser}} />`
+    );
+  });
+
+  test('it renders', async function (assert) {
+    assert.dom('[data-test-input-name]').exists();
+    assert.dom('[data-test-input-surname]').exists();
+    assert.dom('[data-test-input-username]').exists();
+    assert.dom('[data-test-input-password]').exists();
+    assert.dom('[data-test-input-email]').exists();
+    assert.dom('[data-test-input-blik-number]').exists();
+    assert.dom('[data-test-input-bank-account-number]').exists();
+    assert.dom('[data-test-input-revolut]').exists();
+    assert.dom('[data-test-input-paypal]').exists();
+    assert.dom('[data-test-button-edit]').exists();
+  });
+
+  test('correct values', async function (assert) {
+    await render(
+      hbs`<AccountSettings::Details @currentUser={{this.currentUser}} />`
+    );
+    assert.dom('[data-test-input-name]').hasValue(`${this.currentUser.name}`);
+    assert
+      .dom('[data-test-input-surname]')
+      .hasValue(`${this.currentUser.surname}`);
+    assert
+      .dom('[data-test-input-username]')
+      .hasValue(`${this.currentUser.username}`);
+    assert
+      .dom('[data-test-input-password]')
+      .hasValue(`${this.currentUser.password}`);
+    assert.dom('[data-test-input-email]').hasValue(`${this.currentUser.email}`);
+    assert
+      .dom('[data-test-input-blik-number]')
+      .hasValue(`${this.currentUser.blikNumber}`);
+    assert.dom('[data-test-input-bank-account-number]').hasValue(``);
+    assert
+      .dom('[data-test-input-revolut]')
+      .hasValue(`${this.currentUser.revolutUsername}`);
+    assert.dom('[data-test-input-paypal]').hasValue(``);
+  });
+
+  test('edit mode disabled', async function (assert) {
+    assert.dom('[data-test-input-name]').hasAttribute('disabled');
+    assert.dom('[data-test-input-surname]').hasAttribute('disabled');
+    assert.dom('[data-test-input-username]').hasAttribute('disabled');
+    assert.dom('[data-test-input-password]').hasAttribute('disabled');
+    assert.dom('[data-test-input-email]').hasAttribute('disabled');
+    assert.dom('[data-test-input-blik-number]').hasAttribute('disabled');
+    assert
+      .dom('[data-test-input-bank-account-number]')
+      .hasAttribute('disabled');
+    assert.dom('[data-test-input-revolut]').hasAttribute('disabled');
+    assert.dom('[data-test-input-paypal]').hasAttribute('disabled');
+    assert.dom('[data-test-button-edit]').exists();
+  });
+
+  test('edit mode enabled', async function (assert) {
+    await click('[data-test-button-edit]');
+
+    assert.dom('[data-test-input-name]').hasNoAttribute('disabled');
+    assert.dom('[data-test-input-surname]').hasNoAttribute('disabled');
+    assert.dom('[data-test-input-username]').hasNoAttribute('disabled');
+    assert.dom('[data-test-input-password]').hasNoAttribute('disabled');
+    assert.dom('[data-test-input-email]').hasNoAttribute('disabled');
+    assert.dom('[data-test-input-blik-number]').hasNoAttribute('disabled');
+    assert
+      .dom('[data-test-input-bank-account-number]')
+      .hasNoAttribute('disabled');
+    assert.dom('[data-test-input-revolut]').hasNoAttribute('disabled');
+    assert.dom('[data-test-input-paypal]').hasNoAttribute('disabled');
+    assert.dom('[data-test-button-edit]').doesNotExist();
+    assert.dom('[data-test-button-discard]').exists();
+    assert.dom('[data-test-button-save]').exists();
+  });
+
+  test.skip('edit data', async function (assert) {
+    await click('[data-test-button-edit]');
+
+    await fillIn('[data-test-input-name]', 'Wojciech');
+    await fillIn('[data-test-input-surname]', 'Suchodolski');
+    await fillIn('[data-test-input-email]', 'wojtekzbombasu@gmail.com');
+    await waitFor('[data-test-button-save]');
+    await click('[data-test-button-save]');
+
+    assert.dom('[data-test-input-name]').hasValue('Wojciech');
+    assert.dom('[data-test-input-surname]').hasValue('Suchodolski');
+    assert
+      .dom('[data-test-input-username]')
+      .hasValue(`${this.currentUser.username}`);
+    assert
+      .dom('[data-test-input-password]')
+      .hasValue(`${this.currentUser.password}`);
+    assert.dom('[data-test-input-email]').hasValue('wojtekzbombasu@gmail.com');
+    assert
+      .dom('[data-test-input-blik-number]')
+      .hasValue(`${this.currentUser.blikNumber}`);
+    assert.dom('[data-test-input-bank-account-number]').hasValue(``);
+    assert
+      .dom('[data-test-input-revolut]')
+      .hasValue(`${this.currentUser.revolutUsername}`);
+    assert.dom('[data-test-input-paypal]').hasValue(``);
+
+    assert.dom('[data-test-input-name]').hasAttribute('disabled');
+    assert.dom('[data-test-input-surname]').hasAttribute('disabled');
+    assert.dom('[data-test-input-username]').hasAttribute('disabled');
+    assert.dom('[data-test-input-password]').hasAttribute('disabled');
+    assert.dom('[data-test-input-email]').hasAttribute('disabled');
+    assert.dom('[data-test-input-blik-number]').hasAttribute('disabled');
+    assert
+      .dom('[data-test-input-bank-account-number]')
+      .hasAttribute('disabled');
+    assert.dom('[data-test-input-revolut]').hasAttribute('disabled');
+    assert.dom('[data-test-input-paypal]').hasAttribute('disabled');
+    assert.dom('[data-test-button-edit]').exists();
+    assert.dom('[data-test-button-discard]').doesNotExist();
+    assert.dom('[data-test-button-save]').doesNotExist();
+  });
+
+  test.skip('fields validation', async function (assert) {
+    await click('[data-test-button-edit]');
+
+    assert.dom('[data-test-input-name]').hasNoAttribute('disabled');
+    assert.dom('[data-test-input-surname]').hasNoAttribute('disabled');
+    assert.dom('[data-test-input-username]').hasNoAttribute('disabled');
+    assert.dom('[data-test-input-password]').hasNoAttribute('disabled');
+    assert.dom('[data-test-input-email]').hasNoAttribute('disabled');
+    assert.dom('[data-test-input-blik-number]').hasNoAttribute('disabled');
+    assert
+      .dom('[data-test-input-bank-account-number]')
+      .hasNoAttribute('disabled');
+    assert.dom('[data-test-input-revolut]').hasNoAttribute('disabled');
+    assert.dom('[data-test-input-paypal]').hasNoAttribute('disabled');
+    assert.dom('[data-test-button-edit]').doesNotExist();
+    assert.dom('[data-test-button-discard]').exists();
+    assert.dom('[data-test-button-save]').exists();
+  });
+});
