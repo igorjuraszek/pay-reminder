7997004da76f703d5ac50c71c1aa5861f9986f5c
Adam Woźny <wozny1989@gmail.com>
2022-07-21 14:00:29 +0200

    fix: relationships

--- app/contribution-user/model.js
+++ app/contribution-user/model.js
@@ -1,8 +1,8 @@
 import Model, { attr, belongsTo } from '@ember-data/model';
 
 export default class ContributionUserModel extends Model {
-  @belongsTo('user') contributor;
-  @belongsTo('contribution') contribution;
+  @belongsTo('user', { autoSave: true }) contributor;
+  @belongsTo('contribution', { autoSave: true }) contribution;
   @attr('number') amount;
   @attr('boolean') isPaid;
 }
